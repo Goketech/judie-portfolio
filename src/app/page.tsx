72a8392f4a2867ad08c220321ff630b2dec8dfda
Modupe Akanni <modupe775@gmail.com>
2024-09-13 08:58:07 +0100

    feat: add hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Nav from "@/components/Nav/Nav";
+import Hero from "@/components/Hero/Hero";
 
 export default function Home() {
   return (
     <div>
       <Nav />
+      <Hero />
     </div>
   );
 }
---
+++ src/components/Hero/Hero.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='flex flex-col items-center mt-[90px]'>
+        <p className='mb-[16px]'>AVAILABLE FOR NEW PROJECTS</p>
+        <h1 className='font-medium text-[72px] text-center max-w-[700px]'>PRODUCT & VISUAL DESIGNER</h1>
+        <p className='mt-[16px]'>I&apos;M A DIGITAL DESIGNER  PASSIONATE ABOUT CREATING MEMORABLE EXPERIENCES.</p>
+        <h5 className='border-b border-[#FF83B1] text-[#ED7377F9] mt-[18px]'>LET&apos; WORK TOGETHER</h5>
+    </div>
+  )
+}
+
+export default Hero
