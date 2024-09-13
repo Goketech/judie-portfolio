199ef4225047f06782866187754dad96800ce55d
Modupe Akanni <modupe775@gmail.com>
2024-09-13 23:43:04 +0100

    feat: add garage project

--- src/app/work/the-garage/page.tsx
+++ src/app/work/the-garage/page.tsx
@@ -9,15 +9,15 @@ function Work() {
                     <Image src="/arrow-left.svg" alt="arrow" width={32} height={32} />
                 </div>
             </Link>
-            <p className="text-[14px] leading-[21px] text-[#d9d9d9] mb-[4px]">Mobile App Design</p>
-            <h1 className="text-[32px] leading-[38px] font-medium">Letters App</h1>
-            <p className="text-[#d9d9d9] text-[20px] leading-[32px] max-w-[814px] mt-[16px] mb-[32px]">Design a social media app called Letters. Each persons page is a set of beautiful Letters. Long form, like Blog Posts, but with Photos. AI assisted writing. Letters must always be addressed to some. Letters can have letter replies.</p>
+            <p className="text-[14px] leading-[21px] text-[#d9d9d9] mb-[4px]">Website Design</p>
+            <h1 className="text-[32px] leading-[38px] font-medium">The Garage</h1>
+            <p className="text-[#d9d9d9] text-[20px] leading-[32px] max-w-[814px] mt-[16px] mb-[32px]"> Design a simple and user-friendly online store, focusing on a fast and streamlined shopping experience. This will be a single-seller platform showcasing products and checkout experience.</p>
             <div className="flex gap-[44px]">
                 <div className="flex flex-col gap-[4px]">
                     <h5 className="text-[18px] font-medium leading-[26px]">Design Process</h5>
                     <p className="text-[16px] leading-[26px] text-[#d9d9d9]">Ux Research</p>
                     <p className="text-[16px] leading-[26px] text-[#d9d9d9]">Ui Design</p>
-                    <p className="text-[16px] leading-[26px] text-[#d9d9d9]">Usability testing</p>
+                    <p className="text-[16px] leading-[26px] text-[#d9d9d9]">Competitor analysis</p>
                 </div>
                 <div className="flex flex-col gap-[4px]">
                     <h5 className="text-[18px] font-medium leading-[26px]">Tools Used</h5>
@@ -26,19 +26,16 @@ function Work() {
             </div>
             <div className="flex flex-col items-center mt-[100px] gap-[40px]">
                 <div className="border border-[#333333] p-[60px] rounded-[12px] transition-colors duration-300 hover:bg-[#333333]">
-                    <Image src="/letters-app-1.jpg" alt="letters-app" width={708} height={477} />
+                    <Image src="/the-garage-1.jpg" alt="the-garage" width={708} height={477} />
                 </div>
                 <div className="border border-[#333333] p-[60px] rounded-[12px] transition-colors duration-300 hover:bg-[#333333]">
-                    <Image src="/letters-app-2.jpg" alt="letters-app" width={708} height={477} />
+                    <Image src="/the-garage-2.jpg" alt="the-garage" width={708} height={477} />
                 </div>
                 <div className="border border-[#333333] p-[60px] rounded-[12px] transition-colors duration-300 hover:bg-[#333333]">
-                    <Image src="/letters-app-3.jpg" alt="letters-app" width={708} height={477} />
+                    <Image src="/the-garage-3.jpg" alt="the-garage" width={708} height={477} />
                 </div>
                 <div className="border border-[#333333] p-[60px] rounded-[12px] transition-colors duration-300 hover:bg-[#333333]">
-                    <Image src="/letters-app-4.jpg" alt="letters-app" width={708} height={477} />
-                </div>
-                <div className="border border-[#333333] p-[60px] rounded-[12px] transition-colors duration-300 hover:bg-[#333333]">
-                    <Image src="/letters-app-5.jpg" alt="letters-app" width={708} height={477} />
+                    <Image src="/the-garage-4.jpg" alt="the-garage" width={708} height={477} />
                 </div>
             </div>
         </div>
--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -7,8 +7,8 @@ const Projects = () => {
         <div className="grid grid-cols-2 mx-[100px] gap-x-[24px] gap-y-[40px]">
             <ProjectCard url="/work/shop-easy" image="/image1.jpg" title="Shop Easy" type="Mobile App Design" />
             <ProjectCard url="/work/letters-app" image="/image2.jpg" title="Letters App" type="Mobile App Design" />
-            <ProjectCard url="/work/the-garage" image="/image3.jpg" title="The Garage" type="Mobile Design" />
-            <ProjectCard url="/work/carter-shop" image="/image4.jpg" title="Carter Shop" type="Mobile Design" />
+            <ProjectCard url="/work/the-garage" image="/image3.jpg" title="The Garage" type="Website Design" />
+            <ProjectCard url="/work/carter-shop" image="/image4.jpg" title="Carter Shop" type="Website Design" />
             <ProjectCard url="/work/vita-care" image="/image5.jpg" title="Vita Care" type="Website Design" />
             <ProjectCard url="/work/telex" image="/image6.jpg" title="Telex" type="Webapp Design" />
         </div>
