ace4ef427dfd9ece4eb57004749a23a36e0d7891
Modupe Akanni <modupe775@gmail.com>
2024-09-14 00:30:07 +0100

    fix: code style

--- src/components/About/About.tsx
+++ src/components/About/About.tsx
@@ -5,7 +5,7 @@ const About = () => {
     <div className="flex justify-between px-[100px] py-[50px] mt-[100px] bg-[#000000]">
         <div className="max-w-[529px]">
             <h5 className="text-[36px] leading-[52px] mb-[30px]">About me</h5>
-            <p className="text-[16px] leading-[28px] mb-[20px]">Hello, I'm Florence</p>
+            <p className="text-[16px] leading-[28px] mb-[20px]">Hello, I&apos;m Florence</p>
             <p className="text-[16px] leading-[28px] mb-[20px]">I&apos;m committed to helping companies create beautiful, usable solutions from start to finish</p>
             <p className="text-[16px] leading-[28px] mb-[20px]">With a background in visual design and user experience, I specialize in crafting intuitive and engaging digital products.</p>
             <p className="text-[16px] leading-[28px] mb-[20px]">As a ux designer, I&apos;m passionate about delivering tailored solutions for brands and startups worldwide—from building design systems to conducting user research.</p>
