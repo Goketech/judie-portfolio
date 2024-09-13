ffd037cd4e6b3d54e5e0b91f48c22661f6534956
Modupe Akanni <modupe775@gmail.com>
2024-09-13 08:45:48 +0100

    feat: add nav bar

---
+++ src/components/Nav/Nav.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className='flex justify-between px-[100px] py-[48px]'>
+        <div className='font=medium'>Florence Eze</div>
+        <button className='bg-white px-[24px] py-[8px] rounded-[300px] text-[#0C0C0C]'>Contact</button>
+    </div>
+  )
+}
+
+export default Nav
