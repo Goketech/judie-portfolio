908171263f4492b00e35628ab8e1d72c3946b840
Modupe Akanni <modupe775@gmail.com>
2024-09-14 00:01:21 +0100

    feat: add vita project

--- src/app/work/vita-care/page.tsx
+++ src/app/work/vita-care/page.tsx
@@ -10,8 +10,9 @@ function Work() {
                 </div>
             </Link>
             <p className="text-[14px] leading-[21px] text-[#d9d9d9] mb-[4px]">Website Design</p>
-            <h1 className="text-[32px] leading-[38px] font-medium">Carter Shop</h1>
-            <p className="text-[#d9d9d9] text-[20px] leading-[32px] max-w-[814px] mt-[16px] mb-[32px]">Design a simple and user-friendly online store, focusing on a fast and streamlined shopping experience. This will be a single-seller platform showcasing products and checkout experience.</p>
+            <h1 className="text-[32px] leading-[38px] font-medium">Vita Care Web App</h1>
+            <p className="text-[#d9d9d9] text-[20px] leading-[32px] max-w-[814px] mt-[16px] mb-[32px]">Vitacare is a start up company founded with the vision to simplify healthcare. Vitacare is looking to build a web app that will help connect healthcare specialists with users online without the hassle of standing in long queues.
+                To develop a mobile app that enables users to efficiently find, compare, select, and contact healthcare practitioners</p>
             <div className="flex gap-[44px]">
                 <div className="flex flex-col gap-[4px]">
                     <h5 className="text-[18px] font-medium leading-[26px]">Design Process</h5>
@@ -26,19 +27,10 @@ function Work() {
             </div>
             <div className="flex flex-col items-center mt-[100px] gap-[40px]">
                 <div className="border border-[#333333] p-[60px] rounded-[12px] transition-colors duration-300 hover:bg-[#333333]">
-                    <Image src="/carter-shop-1.jpg" alt="carter-shop" width={708} height={477} />
+                    <Image src="/vita-care-1.jpg" alt="vita-care" width={708} height={477} />
                 </div>
                 <div className="border border-[#333333] p-[60px] rounded-[12px] transition-colors duration-300 hover:bg-[#333333]">
-                    <Image src="/carter-shop-2.jpg" alt="carter-shop" width={708} height={477} />
-                </div>
-                <div className="border border-[#333333] p-[60px] rounded-[12px] transition-colors duration-300 hover:bg-[#333333]">
-                    <Image src="/carter-shop-3.jpg" alt="carter-shop" width={708} height={477} />
-                </div>
-                <div className="border border-[#333333] p-[60px] rounded-[12px] transition-colors duration-300 hover:bg-[#333333]">
-                    <Image src="/carter-shop-4.jpg" alt="carter-shop" width={708} height={477} />
-                </div>
-                <div className="border border-[#333333] p-[60px] rounded-[12px] transition-colors duration-300 hover:bg-[#333333]">
-                    <Image src="/carter-shop-5.jpg" alt="carter-shop" width={708} height={477} />
+                    <Image src="/vita-care-2.jpg" alt="vita-care" width={708} height={477} />
                 </div>
             </div>
         </div>
