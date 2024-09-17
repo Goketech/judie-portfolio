650b349d088b445915e197d7ce6366fef2cb17d7
Modupe Akanni <modupe775@gmail.com>
2024-09-17 15:05:14 +0100

    fix: ui

--- src/components/ProjectCard/ProjectCard.tsx
+++ src/components/ProjectCard/ProjectCard.tsx
@@ -11,7 +11,7 @@ interface ProjectCardProps {
 const ProjectCard: React.FC<ProjectCardProps> = ({ image, type, title, url }) => {
   return (
     <Link href={url}>
-      <div className="group cursor-pointer">
+      <div className="group cursor-pointer w-full max-w-[532px] mx-auto">
         <div className="border border-[#333333] p-[34px] rounded-lg transition-colors duration-300 group-hover:bg-[#121212] group-hover:border-0">
           <Image className="rounded-lg" src={image} alt="design-image" width={464} height={469} />
         </div>
