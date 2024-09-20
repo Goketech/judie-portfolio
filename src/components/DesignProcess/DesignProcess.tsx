7b4af8ce96b3519edf8d00365671cd8de8283f7d
Modupe Akanni <modupe775@gmail.com>
2024-09-20 17:41:30 +0100

    feat: update font

--- src/components/DesignProcess/DesignProcess.tsx
+++ src/components/DesignProcess/DesignProcess.tsx
@@ -3,8 +3,8 @@ import ProcessCard from '../ProcessCard/ProcessCard'
 const DesignProcess = () => {
     return (
         <div className='max-w-screen-2xl xl:mx-auto mt-[100px]'>
-            <p className='mx-[22px] md:mx-[100px] text-[#FF5956]  text-[14px] leading-[18px]'>What we do</p>
-            <h3 className='mx-[22px] md:mx-[100px] text-[24px] md:text-[32px] leading-[36px] md:leading-[48px] mb-[40px] md:mb-[80px]'>Bringing Your Ideas to life with thoughtful design and user-focused solutions.</h3>
+            <p className='font-jarkata-sans mx-[22px] md:mx-[100px] text-[#FF5956]  text-[14px] leading-[18px]'>What we do</p>
+            <h3 className='font-jarkata-sans mx-[22px] md:mx-[100px] text-[24px] md:text-[32px] leading-[36px] md:leading-[48px] mb-[40px] md:mb-[80px]'>Bringing Your Ideas to life with thoughtful design and user-focused solutions.</h3>
             <div className='flex flex-col px-[4px] mx-[22px] md:mx-0 md:px-0 md:items-center gap-[24px]'>
                 <h2 className='text-[24px] text-center md:text-[40px] leading-[52px]'>MY DESIGN PROCESS</h2>
                 <ProcessCard image="/research-icon.svg" title="Research" text="I begin by thoroughly understanding the problem, the target audience, and the market. This phase ensures that my designs are informed by real-world insights and are tailored to meet specific needs." />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,9 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        "jarkata-sans": "var(--font-jakarta-sans)",
+      },
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
