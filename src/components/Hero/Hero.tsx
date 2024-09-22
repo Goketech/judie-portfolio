7bef3a5f485b7df56332a92182ec53f9f681777c
Modupe Akanni <modupe775@gmail.com>
2024-09-22 13:30:29 +0100

    fix: space

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,7 +1,7 @@
 
 const Hero = () => {
   return (
-    <div className='flex flex-col items-center mt-[90px]'>
+    <div className='flex flex-col mx-[22px] md:mx-0 items-center mt-[90px]'>
         <p className='mb-[18px] leading-[19px]'>AVAILABLE FOR NEW PROJECTS</p>
         <h1 className='font-medium text-[38px] md:text-[72px] text-center max-w-[345px] md:max-w-[700px] leading-[45px] md:leading-[80px]'>PRODUCT & VISUAL DESIGNER</h1>
         <p className='mt-[18px] leading-[19px] text-center'>I&apos;M A DIGITAL DESIGNER  PASSIONATE ABOUT CREATING MEMORABLE EXPERIENCES.</p>
