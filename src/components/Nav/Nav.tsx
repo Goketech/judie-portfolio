672db8ae0532293dc13d8d84496752c4678758cc
Modupe Akanni <modupe775@gmail.com>
2024-12-08 22:08:57 +0100

    add animation

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -1,9 +1,17 @@
 import Link from "next/link"
+import AnimatedLink from "../AnimatedLinks/index"
 
 const Nav = () => {
   return (
     <div className='flex items-center justify-between px-[50px] py-[24px] md:px-[100px] md:py-[48px]'>
-        <div className='font-medium'>FLORENCE EZE</div>
+        <div className='font-medium'>
+          <AnimatedLink
+                title="FLORENCE EZE"
+                secondTitle="FLORENCE EZE"
+                isLink={true}
+                href="/"
+          />
+        </div>
         <Link href='#contact'><button className='bg-white font-medium transition-colors duration-300 hover:bg-transparent hover:text-white px-[24px] py-[8px] rounded-[300px] text-[#0C0C0C]'>Contact</button></Link>
     </div>
   )
